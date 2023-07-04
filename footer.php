574947098befd24dc52b14a146a4b409185fc866
Comsci Jomar <Jomarlazaro0806@gmail.com>
2023-07-04 10:20:58 +0800

    upload

---
+++ footer.php
@@ -0,0 +1,18 @@
+<center>
+	<footer>
+		<?php 
+			$pageID = 1; // Define the value of $pageID here
+
+			if($pageID == 1) {
+		?>
+			<p style="color: #FFFFFF">TCSF M-Learning Copyright 2023</p>
+		<?php
+			} else {
+		?>
+			<p>TCSF M-Learning Copyright 2023</p>
+		<?php
+			}
+		?>
+		<!-- <p>Programmed by: UNKOWN</p> -->
+	</footer>
+</center>
